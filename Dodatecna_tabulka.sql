c5980bc1c630d147c2f20df3a2ae9a6beb1ad3a0
Eliška Špilarová <164064165+EliskaSpilarova@users.noreply.github.com>
2025-02-09 19:31:56 +0100

    Dodatecna_tabulka.sql
    Tabulka s HDP, GINI koeficientem a populací dalších evropských států 

---
+++ Dodatecna_tabulka.sql
@@ -0,0 +1,22 @@
+CREATE TABLE EuropeanCountriesData (
+    country VARCHAR(100),
+    year INT,
+    gdp NUMERIC(15, 2),
+    gini NUMERIC(5, 2),
+    population BIGINT
+);
+INSERT INTO EuropeanCountriesData (country, year, gdp, gini, population)
+SELECT 
+    country,
+    year,
+    gdp,
+    gini,
+    population
+FROM 
+    economies
+WHERE 
+    country IN ('Germany', 'France', 'Italy', 'Spain', 'Netherlands', 'Sweden', 'Poland', 'Belgium', 'Austria', 
+	'Denmark', 'Finland', 'Greece', 'Portugal', 'Ireland', 'Czechia', 'Romania', 'Hungary', 'Slovakia', 'Luxembourg', 
+	'Bulgaria', 'Slovenia', 'Lithuania', 'Latvia', 'Cyprus', 'Estonia', 'Malta');
+
+SELECT * FROM EuropeanCountriesData;
